b33ecaf06186947022d70f8bd0c47cb57322909d
Damien Aguirre <daguirr6@gmu.edu>
2026-01-02 21:52:15 -0500

    feat: initialize Spring Boot backend

---
+++ backend/src/main/java/com/daguirr/umatraininglab/UmaTrainingLabApplication.java
@@ -0,0 +1,13 @@
+package com.daguirr.umatraininglab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmaTrainingLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UmaTrainingLabApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/daguirr/umatraininglab/UmaTrainingLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daguirr.umatraininglab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UmaTrainingLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
